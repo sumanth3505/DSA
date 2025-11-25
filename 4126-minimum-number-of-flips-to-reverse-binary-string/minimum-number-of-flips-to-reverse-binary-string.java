6344934f0dc1ac34fb9b0f9d373e0117ef70dde5
sumanth3505 <sumanth3505@gmail.com>
2025-11-26 00:03:04 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (33.04%) - LeetSync

--- 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
+++ 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumFlips(int n) {
-        StringBuilder f = new StringBuilder("");
+       /* StringBuilder f = new StringBuilder("");
         StringBuilder l = new StringBuilder("");
         int c=0;
        while(n!=0){
@@ -10,6 +10,13 @@ class Solution {
        }
        for(int i=0;i<f.length();i++){
         if(f.charAt(i)!=l.charAt(i))c++;
+       }*/
+       String str=Integer.toBinaryString(n);
+       int i=0,j=str.length()-1,c=0;
+       while(i<j){
+        if(str.charAt(i)!=str.charAt(j))c+=2;
+        i++;
+        j--;
        }
        return c;
     }
