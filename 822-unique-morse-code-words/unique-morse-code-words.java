714418586607aabd5b2f5063638d7bf505e8b8d3
sumanth3505 <sumanth3505@gmail.com>
2026-01-21 21:21:57 +0530

    Time: 2 ms (98.68%) | Memory: 43.4 MB (62.65%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -4,16 +4,15 @@ class Solution {
         HashSet<String> set=new HashSet<>();
       
         for(String word:words){
-            StringBuffer s=new StringBuffer("");
-            for(int i=0;i<word.length();i++){
-                int ind=word.charAt(i)-'a';
-                s.append(arr[ind]);
+            StringBuffer s=new StringBuffer();
+            for(char c: word.toCharArray()){
+                
+                s.append(arr[c-'a']);
             
             }
-            if (set.contains(s))continue;
-            else{
+            
              set.add(s.toString());
-            }
+            
         }
         return set.size();
     }
