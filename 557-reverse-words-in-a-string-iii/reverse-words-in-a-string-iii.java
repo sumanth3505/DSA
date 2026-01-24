92e6f485a638bd77e0932d5b7b043305bf57aecf
sumanth3505 <sumanth3505@gmail.com>
2026-01-24 16:20:17 +0530

    Time: 10 ms (28.48%) | Memory: 47.1 MB (34.32%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,16 +1,31 @@
 class Solution {
-    public String reverseWords(String s) {
-        String[] s1=s.split(" ");
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<s1.length;i++){
-            StringBuilder sb=new StringBuilder(s1[i]);
-            sb.reverse();
-            str.append(sb.toString());
-            if(i<s1.length-1){
-                str.append(" ");
-            }
+    public String reverseWords(String str) {
+        // String[] s1=s.split(" ");
+        // StringBuilder str=new StringBuilder();
+        // for(int i=0;i<s1.length;i++){
+        //     StringBuilder sb=new StringBuilder(s1[i]);
+        //     sb.reverse();
+        //     str.append(sb.toString());
+        //     if(i<s1.length-1){
+        //         str.append(" ");
+        //     }
+        // }
+        // return str.toString();
+       StringBuilder s=new StringBuilder(str);
+       int prev=0;
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)==' '){
+            String sub=s.substring(prev,i);
+            sub=new StringBuilder(sub).reverse().toString();
+            s.replace(prev,i,sub);
+            prev=i+1;
         }
-        return str.toString();
-       
+        if(i==s.length()-1){
+            String sub=s.substring(prev,i+1);
+            sub=new StringBuilder(sub).reverse().toString();
+            s.replace(prev,i+1,sub);
+        }
+       }
+       return s.toString();
     }
 }
