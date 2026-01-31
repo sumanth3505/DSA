4a3b633c2e6385d02568df76beae7a3ff16fc340
sumanth3505 <sumanth3505@gmail.com>
2026-01-31 22:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (74.47%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder s=new StringBuilder();
+        for(char c :address.toCharArray()){
+           if(c=='.'){
+            s.append("[.]");
+           }
+           else{
+            s.append(c);
+           }
+        }
+        return s.toString();
+    }
+}
